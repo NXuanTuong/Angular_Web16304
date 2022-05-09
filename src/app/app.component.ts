dddabadf1170c1e110250be046c1620d0773be68
NXuanTuong <tuongbabynhok@gmail.com>
2022-05-09 11:17:11 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,19 @@ export class AppComponent {
   name  = 'Xuan Tuong';
   class = 'Web16304';
 
-  // Phần định nghĩa, logic, đặt giá trị cho biến 
+  // Phần định nghĩa giá trị cho biến, logic, sử dụng ở html 
+  // Kiểu dữ liệu
+  students = [
+    {
+      id: "1",
+      name: "xuantuong01",
+      status: 0
+    },
+    {
+      id: "2",
+      name: "xuantuong02",
+      status: 1
+    },
+
+  ]
 }
